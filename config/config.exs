3f256e14b8154e3203b50b42f2c981facfd32ca5
brainnzzz <andrew.braim@gmail.com>
2017-09-04 22:47:09 +0300

    user model and jwt auth

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :guardian, Guardian,
+  issuer: "TrelloWeb",
+  ttl: { 3, :days },
+  verify_issuer: true,
+  secret_key: "UggiTNkHT9IfY18ZSsVT6R4r6Jw6On",
+  serializer: Trello.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Trello.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.0"},
+      {:guardian, ">= 0.9.0"},
     ]
   end
 
